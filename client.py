665299e59ca455a723a74b21230e46469da71378
Mateusz <63258155+MateuszZelaszczyk@users.noreply.github.com>
2022-06-04 17:43:02 +0200

    connect client and server add second player

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from network import Network
 width = 600
 height = 700
 
@@ -16,13 +17,16 @@ class  Player():
         self.width = width
         self.color = color
         self.image = pygame.transform.scale(pygame.image.load("cma.jpg"),(80,80))
+        self.image2 = pygame.transform.scale(pygame.image.load("cma2.jpg"),(80,80))
         self.rect = (x,y,width,height)
         self.vel = 3
 
-    def draw(self,win):
+    def draw(self,win, number):
         pygame.draw.rect(win, self.color, self.rect)
-        win.blit(self.image, self.rect)
-
+        if(number=="1"):
+            win.blit(self.image, self.rect)
+        elif(number=="2"):
+            win.blit(self.image2, self.rect)
     def move(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
@@ -33,24 +37,46 @@ class  Player():
             self.y -=self.vel
         if keys[pygame.K_DOWN]:
             self.y +=self.vel
+
+        self.update()
+
+    def update(self):
         self.rect = (self.x, self.y, self.width, self.height)
-def Window(win,player):
+
+def read_pos(str):
+    str=str.split(",")
+    return int(str[0]), int(str[1])
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])
+
+def window(win,player, player2):
     win.fill((255,255,255))
-    player.draw(win)
+    player.draw(win,"1")
+    player2.draw(win,"2")
     pygame.display.update()
 
 def main():
     pygame.init()
     run = True
-    p = Player(50,50,80,80,(255,255,255))
+    n=Network()
+    startPos = read_pos(n.getPos())
+    p = Player(startPos[0], startPos[1],80,80,(255,255,255))
+    p2 = Player(0,0,80,80,(255,255,255))
     clock = pygame.time.Clock()
+
     while run:
         clock.tick(60)
+        p2Pos = read_pos(n.send(make_pos((p.x, p.y))))
+        p2.x = p2Pos[0]
+        p2.y = p2Pos[1]
+        p2.update()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
                 sys.exit()
         p.move()
-        Window(win,p)
+        window(win,p,p2)
 main()
--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 from _thread import *
-from distutils.log import error
 import  socket
 import sys
  
@@ -16,25 +15,45 @@ except socket.error as e:
 s.listen(2)
 print("Waiting for a connection, Server Started")
 
-def threaded_client(connect):
+def read_pos(str):
+    str=str.split(",")
+    return int(str[0]), int(str[1])
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])
+    
+pos=[(0,0),(100,100)]
+
+def threaded_client(connect, player):
+    connect.send(str.encode(make_pos(pos[player])))
     reply=""
     while True:
         try:
-            data = connect.recv(2048)
-            reply = data.decode("utf-8")
-
+            data = read_pos(connect.recv(2048).decode())
+            pos[player] = data
+            
             if not data:
                 print("Disconnected")
                 break
             else:
-                print("Recieved:", reply)
+                if player == 1:
+                    reply = pos[0]
+                else:
+                    reply = pos[1]
+
+                print("Recieved:", data)
                 print("Sending: ", reply)
-            connect.sendall(str.encode(reply))
+            connect.sendall(str.encode(make_pos(reply)))
         except:
             break
 
+    print("Lost connection")
+    connect.close()
+
+currentPlayer =0
 while True:
     connect, addr = s.accept()
     print("Connected to:", addr)
 
-    start_new_thread(threaded_client, (connect,) )
+    start_new_thread(threaded_client, (connect, currentPlayer) )
+    currentPlayer +=1
