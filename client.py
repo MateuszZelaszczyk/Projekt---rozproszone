4d7c48e58e5ea78aa46e5d9e653b22cc7e32dc50
martamazurkiewicz <marta@mazurkiewi.cz>
2022-06-10 17:30:21 +0200

    Map objects are sent from server to client

--- client.py
+++ client.py
@@ -2,16 +2,19 @@ import sys
 from random import randrange
 import pygame
 from network import Network
+
 width = 600
 height = 700
 
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
 plant_image = pygame.transform.scale(pygame.image.load("plant.png"), (40, 40))
+plant_image2 = pygame.transform.scale(pygame.image.load("plant2.png"), (40, 40))
 plants = dict()
 clientNumber = 0
 
-class  Player():
+
+class Player:
     def __init__(self, x, y, width, height, color):
         self.x = x
         self.y = y
@@ -20,26 +23,26 @@ class  Player():
         self.color = color
         self.image = pygame.transform.scale(pygame.image.load("cma.jpg"), (60, 60))
         self.image2 = pygame.transform.scale(pygame.image.load("cma2.jpg"), (60, 60))
-        self.rect = (x,y,width,height)
+        self.rect = (x, y, width, height)
         self.vel = 3
 
-
-    def draw(self,win, number):
+    def draw(self, win, number):
         pygame.draw.rect(win, self.color, self.rect)
-        if(number=="1"):
+        if number == "1":
             win.blit(self.image, self.rect)
-        elif(number=="2"):
+        elif number == "2":
             win.blit(self.image2, self.rect)
+
     def move(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
-            self.x -=self.vel
+            self.x -= self.vel
         if keys[pygame.K_RIGHT]:
-            self.x +=self.vel
+            self.x += self.vel
         if keys[pygame.K_UP]:
-            self.y -=self.vel
+            self.y -= self.vel
         if keys[pygame.K_DOWN]:
-            self.y +=self.vel
+            self.y += self.vel
 
         self.update()
 
@@ -47,17 +50,16 @@ class  Player():
         self.rect = (self.x, self.y, self.width, self.height)
 
 
-def draw_plant(win, has_photo, coordinates):
-    position = [coordinates[0], coordinates[1], 20, 20]
+def draw_plant(win, plant_data):
+    position = [plant_data[1], plant_data[2], 40, 40]
     pygame.draw.rect(win, (50, 200, 50), position)
-    if has_photo:
+    if plant_data[0]:
         win.blit(plant_image, position)
-    #else:
-        #win.blit(position)
-        #pass
-    #win.blit(plant_image, position)
+    else:
+        win.blit(plant_image2, position)
 
-def read_map_postions(strs):
+
+def read_map_positions(strs):
     strs = strs.split(";")
     player2_pos = read_pos(strs[0])
     strs.pop(0)
@@ -71,25 +73,29 @@ def read_positions(strs):
     objects = str.split(",")
     return player2_pos, objects
 
+
 def read_pos(str):
     str = str.split(",")
     return int(str[0]), int(str[1])
 
-def read_plant_pos(str):
+
+def make_plant_pos(str):
     str = str.split(",")
-    plants[int(str[0])] = [int(str[1]), int(str[2])]
+    plants[int(str[0])] = [True if randrange(2) else False, int(str[1]), int(str[2])]
+
 
 def make_pos(tup):
     return str(tup[0]) + "," + str(tup[1])
 
 
 def window(win, player, player2):
-    win.fill((255, 255, 255))
-    for map_object in plants:
-        draw_plant(win, True if randrange(2) else False, [map_object[0], map_object[1]])
+    win.fill((34, 139, 34))
+    for i in plants:
+        draw_plant(win, plants[i])
     player.draw(win, "1")
     player2.draw(win, "2")
     pygame.display.update()
+
 
 def delete_objects(objects_keys):
     for key in objects_keys:
@@ -98,13 +104,14 @@ def delete_objects(objects_keys):
         except KeyError as e:
             pass
 
+
 def main():
     pygame.init()
     run = True
-    n=Network()
-    startPos, plants_pos = read_map_postions(n.getPos())
+    n = Network()
+    startPos, plants_pos = read_map_positions(n.get_position())
     for str in plants_pos:
-        read_plant_pos(str)
+        make_plant_pos(str)
     p = Player(int(startPos[0]), int(startPos[1]), 60, 60, (255, 255, 255))
     p2 = Player(0, 0, 60, 60, (255, 255, 255))
     clock = pygame.time.Clock()
@@ -126,4 +133,6 @@ def main():
                 sys.exit()
         p.move()
         window(win, p, p2)
+
+
 main()
--- network.py
+++ network.py
@@ -1,6 +1,5 @@
-from cmath import exp
-import  socket
-from unittest import expectedFailure
+import socket
+
 
 class Network:
     def __init__(self):
@@ -14,16 +13,18 @@ class Network:
         self.addr = (self.server, self.port)
         self.pos = self.connect()
 
-    def getPos(self):
+    def get_position(self):
         return self.pos
 
     def connect(self):
         try:
             self.client.connect(self.addr)
-            return self.client.recv(4096).decode()
-        except:
-            pass
-    def send(self,data):
+            reply = self.client.recv(8192).decode()
+            return reply
+        except socket.error as e:
+            print(e)
+
+    def send(self, data):
         try:
             self.client.send(str.encode(data))
             return self.client.recv(4096).decode()
