17521ff790259e1c11e89dab13bd812fec894f50
martamazurkiewicz <marta@mazurkiewi.cz>
2022-06-10 15:46:31 +0200

    Added order to containers

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ docker stop secondmapserver
 docker stop mongo
 yes | docker container prune
 docker volume rm mongo_db-volume
-docker compose build
+docker compose build --no-cache
 docker compose up
