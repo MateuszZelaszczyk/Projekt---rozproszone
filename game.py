9eb2b9bf441a017014ef6873e49fb1412f26e6fa
martamazurkiewicz <marta@mazurkiewi.cz>
2022-06-10 21:50:41 +0200

    Players see each other only when they are on the same map

--- game.py
+++ game.py
@@ -20,18 +20,18 @@ class Game():
         self.plants[int(plant_data[0])] = [True if randrange(2) else False, plant_data[1], int(plant_data[2]), int(plant_data[3])]
 
     def get_m1_plants(self):
-        m1_plants = dict()
+        m1_keys = []
         for key in self.plants:
             if self.plants[key][1] == 'm1':
-                m1_plants[key] = self.plants[key]
-        return m1_plants
+                m1_keys.append(key)
+        return m1_keys
 
     def get_m2_plants(self):
-        m2_plants = dict()
+        m2_keys = []
         for key in self.plants:
             if self.plants[key][1] == 'm2':
-                m2_plants[key] = self.plants[key]
-        return m2_plants
+                m2_keys.append(key)
+        return m2_keys
 
     def draw_plant(self, win, plant_data):
         position = [plant_data[2], plant_data[3], 40, 40]
@@ -64,20 +64,21 @@ class Game():
         return str[0], int(str[1]), int(str[2])
 
     def make_pos(self, tup):
-        return str(tup[0]) + "," + str(tup[1]) + "," + str(tup[1])
+        return str(tup[0]) + "," + str(tup[1]) + "," + str(tup[2])
 
     def window(self, player, player2):
         self.win.fill((34, 139, 34))
         if player.map == 'm1':
             self.win.blit(self.map1_image, (0, 0))
-            local_plants = self.get_m1_plants()
+            local_plant_keys = self.get_m1_plants()
         else:
             self.win.blit(self.map2_image, (0, 0))
-            local_plants = self.get_m2_plants()
-        for i in local_plants:
-            self.draw_plant(self.win, self.plants[i])
+            local_plant_keys = self.get_m2_plants()
+        for key in local_plant_keys:
+            self.draw_plant(self.win, self.plants[key])
+        if player.map == player2.map:
+            player2.draw(self.win, "2")
         player.draw(self.win, "1")
-        player2.draw(self.win, "2")
         pygame.display.update()
 
     def delete_objects(self, objects_keys):
