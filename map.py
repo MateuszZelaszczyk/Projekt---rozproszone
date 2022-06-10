5cdd93bdbce973ca8e9fbf6c15c13017c2dae476
martamazurkiewicz <marta@mazurkiewi.cz>
2022-06-10 13:54:41 +0200

    Added class for creating map

---
+++ map.py
@@ -0,0 +1,24 @@
+import pygame
+from random import randrange
+
+
+class Map:
+    def __init__(self, width, height):
+        self.height = height
+        self.width = width
+        self.objects = []
+        self.create_map()
+
+    def create_map(self):
+        objects_number = randrange(350, 500)
+        for i in range(objects_number):
+            self.objects.append([randrange(self.width), randrange(self.height)])
+
+    def delete_object(self, x, y):
+        pass
+
+    def get_object_as_str(self, game_object):
+        return str(game_object[0]) + "," + str(game_object[1])
+
+    def get_objects_coordinates_as_str(self):
+        return ';'.join(map(lambda x: self.get_object_as_str(x), self.objects))
