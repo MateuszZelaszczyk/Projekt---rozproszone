f5aa405f68a3dc87d816115017990dbc8db94a5f
martamazurkiewicz <marta@mazurkiewi.cz>
2022-06-11 18:13:20 +0200

    Changed plants number

--- map.py
+++ map.py
@@ -9,11 +9,11 @@ class Map:
         self.create_map()
 
     def create_map(self):
-        objects_number = randrange(230, 250)
+        objects_number = randrange(170, 180)
         for i in range(objects_number):
-            self.objects[i] = ['m1', randrange(0, self.width, 30), randrange(0, self.height, 30)]
+            self.objects[i] = ['m1', randrange(0, self.width, 40), randrange(0, self.height, 40)]
         for i in range(objects_number):
-            self.objects[objects_number+i] = ['m2', randrange(0, self.width, 30), randrange(0, self.height, 30)]
+            self.objects[objects_number+i] = ['m2', randrange(0, self.width, 40), randrange(0, self.height, 40)]
 
     def get_object_as_str(self, key):
         return str(key) + "," + str(self.objects[key][0]) + "," + str(self.objects[key][1]) + "," + str(self.objects[key][2])
