82a2353aeaecf8c3819492e114b542c8f6f89d05
martamazurkiewicz <marta@mazurkiewi.cz>
2022-06-11 17:30:51 +0200

    Removed comments

--- network.py
+++ network.py
@@ -4,12 +4,9 @@ import socket
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        ##< TODO: this should be an individual class as in prodaction this wold be a fixed address, not docker
         hostname = socket.gethostname()
         self.server = socket.gethostbyname(hostname)
         self.port = 5555
-        #self.second_server_port = 5556
-        ##< TODO
         self.addr = (self.server, self.port)
         self.pos = self.connect()
 
